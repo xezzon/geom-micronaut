a83786e78ee008c02382fa0cf7e3605aa96699aa
xezzon <xezzon@outlook.com>
2024-05-11 10:52:56 +0800

    KeyLoader

--- geom-service/geom-service-admin/src/main/java/io/github/xezzon/geom/crypto/KeyManager.java
+++ geom-service/geom-service-admin/src/main/java/io/github/xezzon/geom/crypto/KeyManager.java
@@ -1,31 +1,25 @@
 package io.github.xezzon.geom.crypto;
 
 import cn.hutool.crypto.KeyUtil;
-import cn.hutool.crypto.PemUtil;
 import io.github.xezzon.geom.auth.event.PublicKeyGeneratedEvent;
 import io.github.xezzon.geom.config.GeomConfig.GeomJwtConfig;
+import io.github.xezzon.geom.crypto.service.KeyLoader;
 import io.github.xezzon.tao.observer.ObserverContext;
 import io.micronaut.context.annotation.Context;
 import jakarta.annotation.PostConstruct;
 import jakarta.inject.Singleton;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Path;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.spec.KeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import lombok.extern.slf4j.Slf4j;
 import org.bouncycastle.jce.ECNamedCurveTable;
 import org.bouncycastle.jce.interfaces.ECPrivateKey;
 import org.bouncycastle.jce.spec.ECParameterSpec;
 import org.bouncycastle.jce.spec.ECPublicKeySpec;
 import org.bouncycastle.math.ec.ECPoint;
-import org.bouncycastle.util.io.pem.PemObject;
 
 /**
  * TODO 优化扩展性 以便兼容其他加密算法
@@ -38,9 +32,11 @@ public class KeyManager {
   public static final String ALGORITHM = "EC";
   private final GeomJwtConfig geomJwtConfig;
   private PrivateKey privateKey;
+  private final KeyLoader keyLoader;
 
-  public KeyManager(GeomJwtConfig geomJwtConfig) {
+  public KeyManager(GeomJwtConfig geomJwtConfig, KeyLoader keyLoader) {
     this.geomJwtConfig = geomJwtConfig;
+    this.keyLoader = keyLoader;
     ObserverContext.register(PublicKeyGeneratedEvent.class, this::printPublicKey);
     ObserverContext.register(PublicKeyGeneratedEvent.class, this::savePublicKeyToClasspath);
   }
@@ -52,26 +48,19 @@ public class KeyManager {
   public void loadPrivateKey() {
     /* 获取私钥文件 */
     String privateKeyFilename = geomJwtConfig.getIssuer() + ".pri";
-    URL classpath = getClass().getClassLoader().getResource("");
-    assert classpath != null;
-    Path privateKeyPath = Path.of(privateKeyFilename);
     PublicKey publicKey = null;
     try {
-      File privateKeyFile = Path.of(classpath.toURI())
-          .resolve(privateKeyPath)
-          .toFile();
-      try (InputStream pemStream = new FileInputStream(privateKeyFile)) {
-        /* 从文件中解析私钥 */
-        PemObject pemObject = PemUtil.readPemObject(pemStream);
-        this.privateKey = KeyUtil.generatePrivateKey(ALGORITHM, pemObject.getContent());
-      }
+      byte[] pemContent = keyLoader.read(privateKeyFilename);
+      KeyFactory keyFactory = KeyFactory.getInstance(ALGORITHM);
+      KeySpec keySpec = new X509EncodedKeySpec(pemContent);
+      this.privateKey = keyFactory.generatePrivate(keySpec);
       /* 从私钥中提取公钥 */
       ECParameterSpec ecSpec = ECNamedCurveTable.getParameterSpec("secp256k1");
       ECPoint qPoint = ecSpec.getG().multiply(((ECPrivateKey) this.privateKey).getD());
       ECPublicKeySpec pubSpec = new ECPublicKeySpec(qPoint, ecSpec);
       publicKey = KeyUtil.generatePublicKey(ALGORITHM, pubSpec);
     } catch (Exception ignored) {
-      log.debug("Cannot load private key from file.");
+      log.debug("Cannot load private key.");
     }
     if (this.privateKey == null) {
       /* 获取不到文件或解析不了 则生成一对密钥 */
@@ -79,13 +68,7 @@ public class KeyManager {
       this.privateKey = ecc.getPrivate();
       publicKey = ecc.getPublic();
       /* 保存私钥 */
-      try (FileWriter pemWriter = new FileWriter(
-          Path.of(classpath.toURI()).resolve(privateKeyPath).toFile()
-      )) {
-        PemUtil.writePemObject("PRIVATE KEY", this.privateKey.getEncoded(), pemWriter);
-      } catch (IOException | URISyntaxException ex) {
-        throw new RuntimeException(ex);
-      }
+      keyLoader.write(privateKeyFilename, this.privateKey.getEncoded(), "PRIVATE KEY");
     }
     /* 广播公钥 */
     ObserverContext.post(new PublicKeyGeneratedEvent(publicKey));
@@ -113,18 +96,6 @@ public class KeyManager {
    */
   public void savePublicKeyToClasspath(PublicKeyGeneratedEvent event) {
     String publicKeyFilename = geomJwtConfig.getIssuer() + ".pub";
-    URL classpath = getClass().getClassLoader().getResource("");
-    assert classpath != null;
-    try {
-      File publicKeyFile = Path.of(classpath.toURI())
-          .resolve(Path.of(publicKeyFilename))
-          .toFile();
-      try (FileWriter pemWriter = new FileWriter(publicKeyFile)) {
-        PemUtil.writePemObject("PUBLIC KEY", event.publicKey().getEncoded(), pemWriter);
-        log.info("JWT Public Key save to file: {}", publicKeyFile.getAbsoluteFile());
-      }
-    } catch (URISyntaxException | IOException e) {
-      throw new RuntimeException(e);
-    }
+    keyLoader.write(publicKeyFilename, event.publicKey().getEncoded(), "PUBLIC KEY");
   }
 }
